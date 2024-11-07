d91da284165862f60b472a840b133198b7cf187b
Nicholas <nidj@netcompany.com>
2024-11-07 11:17:13 +0100

    inital domain setup

---
+++ src/RoutePlanning.Domain/Booking/Booking.cs
@@ -0,0 +1,13 @@
+﻿namespace RoutePlanning.Domain.Booking;
+public class Booking
+{
+    public Booking(Guid reference, string origin, string destination)
+    {
+        Reference = reference;
+        Origin = origin;
+        Destination = destination;
+    }
+    public Guid Reference { get; }
+    public string Origin { get; }
+    public string Destination { get; }
+}
---
+++ src/RoutePlanning.Domain/Parcel/Parcel.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace RoutePlanning.Domain.Parcel;
+public class Parcel
+{
+    public Parcel(Guid bookingReference, float weight, float height, float width)
+    {
+        BookingReference = bookingReference;
+        Weight = weight;
+        Height = height;
+        Width = width;
+    }
+
+    public Guid BookingReference { get; set; }
+    public float Weight { get; set; }
+    public float Height { get; set; }
+    public float Width { get; set; }
+
+}
